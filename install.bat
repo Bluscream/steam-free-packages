3bbd79024d591de729ce5cd70cb7b7689a4e5f73
Bluscream <admin@timo.de.vc>
2018-04-09 19:56:29 +0200

    update installer

--- install.bat
+++ install.bat
@@ -4,3 +4,11 @@ start npm install fs
 start npm install steamauth
 start npm install steam-user
 start npm install steam-store
+start npm install steamcommunity
+start npm install jsdom
+start npm install jquery
+start npm install request
+start npm install steamcommunity
+start npm install cloudscraper
+start npm install chalk
+start npm install console-title
